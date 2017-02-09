af685d5d60053288f651452181d368df558de5df
brianfajardo <bmf5@hotmail.com>
2017-02-09 04:17:29 -0500

    finish filter development

--- examples/filter/filter.js
+++ examples/filter/filter.js
@@ -1,1 +1,21 @@
 //working through the filter function
+
+var filter = function (originalArray, callback, optionalThisObject) {
+
+    var filterCallback = callback;
+
+    if (optionalThisObject){
+        filterCallback = callback.bind(optionalThisObject);
+    };
+
+    var filteredArray = []
+
+    for (var i = 0; i < originalArray.length; i++){
+        if (filterCallback(originalArray[i], i, originalArray)){
+            filteredArray.push(originalArray[i])
+        };
+    };
+
+    return filteredArray;
+
+}

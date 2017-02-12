6608c139fbf58ff80072ee792c2e8012ee5b4af9
brianfajardo <bmf5@hotmail.com>
2017-02-11 21:25:54 -0500

    refactoring

--- examples/reduce/reduce.js
+++ examples/reduce/reduce.js
@@ -11,35 +11,31 @@
     // arr.reduce(callback(accumulator, currentValue, currentIndex, array)[, initialValue])
 
 var reduce = function(array, callback, initialValue){
-
     var startingIndex = 0;
     var accumulator = initialValue;
+    var arrayKeys = Object.keys(array)
+    var length = array.length
 
-    //if no initialValue
     if (arguments.length < 3){
-
-        if(Object.keys(array).length === 0){
+        if(arrayKeys.length === 0){
             throw new TypeError('Reduce of empty array with no initial value');
         };
-
-        if (Object.keys(array).length === 1){
-            var objectKeyIndex = Object.keys(array)[0];
+        if (arrayKeys.length === 1){
+            var objectKeyIndex = arrayKeys[0];
             var onlyArrayElement = array[objectKeyIndex];
             return onlyArrayElement;
         };
-        while (startingIndex in array === false && startingIndex < array.length){
+        while (startingIndex in array === false && startingIndex < length){
             startingIndex++;
         };
         accumulator = array[startingIndex];
         startingIndex++;
-
     } else {
-        if (Object.keys(array).length === 0){
+        if (arrayKeys.length === 0){
             return initialValue;
-        }
+        };
     };
-
-    for (var i = startingIndex; i < array.length; i++){
+    for (var i = startingIndex; i < length; i++){
         if (i in array){
             accumulator = callback(accumulator, array[i], i, array);
         };
